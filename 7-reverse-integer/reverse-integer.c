9c021f1a3907b15540298a03d08816628b4e783e
Guilherme Augusto <51034615+guialima29@users.noreply.github.com>
2026-04-15 08:15:31 -0400

    Time: 3 ms (55.01%) | Memory: 9.1 MB (77.26%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,13 @@
+int reverse(int x){
+    int res = 0;
+    while (x != 0) {
+        int pop;
+        pop = x % 10;
+        if (res > INT_MAX / 10 || (res == INT_MAX / 10 && pop > 7)) return 0;
+        if (res < INT_MIN / 10 || (res == INT_MIN / 10 && pop < -8)) return 0;
+        res = res * 10 + pop;
+        x = x / 10;
+    }
+
+    return res;
+}
